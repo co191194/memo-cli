51dd0a567c912833b11991950a628ab1425968a7
co191194 <co191194@outlook.com>
2026-07-06 23:00:29 +0900

    refactor: move Memo type to memo.go

--- main.go
+++ main.go
@@ -3,16 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 )
-
-type Memo struct {
-	ID        int       `json:"id"`
-	Title     string    `json:"title"`
-	Body      string    `json:"body"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
 
 func main() {
 
