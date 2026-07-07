c8646e65b4c3e295f848d9bd9a3047b9f6aef40c
co1119 <100205227+co191194@users.noreply.github.com>
2026-07-07 22:59:09 +0900

    feat: add load and save memos (#16)
    * feat: add load and save memos
    
    * fix: return an empty list only if the target file does not exist file
    
    * fix: avoid truncating memos before save succeeds

--- memo.go
+++ memo.go
@@ -1,6 +1,12 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
 
 type Memo struct {
 	ID        int       `json:"id"`
@@ -8,4 +14,79 @@ type Memo struct {
 	Body      string    `json:"body"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func LoadMemos(path string) ([]Memo, error) {
+
+	path, err := expandPath(path)
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Memo{}, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	var memos []Memo
+	if err := json.NewDecoder(file).Decode(&memos); err != nil {
+		return nil, err
+	}
+
+	return memos, nil
+}
+
+func SaveMemos(path string, memos []Memo) error {
+
+	path, err := expandPath(path)
+	if err != nil {
+		return err
+	}
+
+	dirPath := filepath.Dir(path)
+
+	if err := os.MkdirAll(dirPath, 0755); err != nil {
+		return err
+	}
+
+	tmpFile, err := os.CreateTemp(dirPath, ".memos-*.json")
+	if err != nil {
+		return err
+	}
+	tmpPath := tmpFile.Name()
+	defer os.Remove(tmpPath)
+
+	encoder := json.NewEncoder(tmpFile)
+	encoder.SetIndent("", "  ")
+
+	if err := encoder.Encode(memos); err != nil {
+		tmpFile.Close()
+		return err
+	}
+
+	if err := tmpFile.Close(); err != nil {
+		return err
+	}
+
+	if err := os.Rename(tmpPath, path); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func expandPath(path string) (string, error) {
+	if strings.HasPrefix(path, "~/") {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+
+		path = filepath.Join(home, path[2:])
+	}
+	return path, nil
 }
