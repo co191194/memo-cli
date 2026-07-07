72d64e4fdf38866c9ad2bb86669ccba709240004
co191194 <co191194@outlook.com>
2026-07-07 21:28:38 +0900

    feat: add load and save memos

--- memo.go
+++ memo.go
@@ -1,6 +1,13 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
 
 type Memo struct {
 	ID        int       `json:"id"`
@@ -8,4 +15,67 @@ type Memo struct {
 	Body      string    `json:"body"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func LoadMemos(path string) ([]Memo, error) {
+
+	path, err := expandPath(path)
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return []Memo{}, nil
+	}
+	defer file.Close()
+
+	var memos []Memo
+	if err := json.NewDecoder(file).Decode(&memos); err != nil {
+		return nil, err
+	}
+
+	return memos, nil
+}
+
+func SaveMemos(path string, memos []Memo) error {
+
+	path, err := expandPath(path)
+	if err != nil {
+		return err
+	}
+
+	dirPath := filepath.Dir(path)
+
+	if err := os.MkdirAll(dirPath, 0755); err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Println("JSONファイルが開けません", err)
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+
+	encoder.SetIndent("", "  ")
+
+	if err = encoder.Encode(memos); err != nil {
+		return err
+	}
+	return nil
+}
+
+func expandPath(path string) (string, error) {
+	if strings.HasPrefix(path, "~/") {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+
+		path = filepath.Join(home, path[2:])
+	}
+	return path, nil
 }
